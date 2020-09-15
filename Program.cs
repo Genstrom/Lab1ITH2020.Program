5aac32829b7ca1e075f3c3782430db5d2f67a0f9
Gabriel Enström <59197583+Genstrom@users.noreply.github.com>
2020-09-16 00:57:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,42 +1,38 @@
-﻿using System;
+using System;
 using System.Numerics;
 
 namespace Lab1ITH2020.Program
 {
-    class Program
+    internal class Program
     {
-
-
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-
-            BigInteger coloredNumber = 0;
             BigInteger total = 0;
-            int counter = 0;
+
             Console.WriteLine("Please insert a string of characters");
 
-            string userInput = Console.ReadLine();
+            var userInput = Console.ReadLine();
             Console.WriteLine();
 
-            for (int i = 0; i < userInput.Length; i++)
+            for (var i = 0; i < userInput.Length; i++)
             {
-                counter = 0;
-                for (int y = i + 1; y < userInput.Length; y++)
-                {
-
-                    if (int.TryParse(userInput[y].ToString(), out int tryToParse))
+                var counter = 0;
+                for (var y = i + 1; y < userInput.Length; y++)
+                    if (int.TryParse(userInput[y].ToString(), out var tryToParse))
                     {
-
                         counter++;
 
                         if (userInput[i] == userInput[y])
                         {
-
+                            BigInteger coloredNumber = 0;
                             coloredNumber = BigInteger.Parse(userInput.Substring(i, counter + 1));
-                            total += coloredNumber;
-                            Console.ForegroundColor = ConsoleColor.White; Console.Write(userInput.Substring(0, i));
-                            Console.ForegroundColor = ConsoleColor.Red; Console.Write(userInput.Substring(i, counter + 1));
-                            Console.ForegroundColor = ConsoleColor.White; Console.Write(userInput.Substring(y + 1));
+                            total = total + coloredNumber;
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(userInput.Substring(0, i));
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write(userInput.Substring(i, counter + 1));
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write(userInput.Substring(y + 1));
                             Console.WriteLine();
                             break;
                         }
@@ -45,8 +41,8 @@ namespace Lab1ITH2020.Program
                     {
                         break;
                     }
-                }
             }
+
             Console.WriteLine($"The total of the colored numbers put together equals the number {total}");
         }
     }
